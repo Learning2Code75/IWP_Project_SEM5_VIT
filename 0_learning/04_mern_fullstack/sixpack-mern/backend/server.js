2128738a950f77440c7c53477dda6a761dcae42c
NeelChoksi <neelchoksi1611@gmail.com>
2021-11-06 06:38:55 +0530

    1hr 6pack mern

--- 0_learning/04_mern_fullstack/sixpack-mern/backend/server.js
+++ 0_learning/04_mern_fullstack/sixpack-mern/backend/server.js
@@ -1,9 +1,10 @@
 const app = require('./app');
 
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
+const connectToDatabase = require('./config/database');
 
-dotenv.config({path:'backend/config/config.env'})
-
+dotenv.config({path:'backend/config/config.env'});
+connectToDatabase();
 app.listen(process.env.PORT,()=>{
 	console.log(`Server is running on PORT ${process.env.PORT}`);
 });
